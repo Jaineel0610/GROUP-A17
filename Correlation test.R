2df90609df98a96981349cc1af8998909f3f5adb
Shanvitha.Vanam <sv24acc@herts.ac.uk>
2025-12-12 15:07:53 +0000

    Fix missing newline at end of file

--- Correlation test.R
+++ Correlation test.R
@@ -2,4 +2,4 @@
 cor_test <- cor.test(data_clean$price, data_clean$rating, method = "pearson")
 cat("\nPearson Correlation Test:\n")
 cat("Correlation coefficient (r):", round(cor_test$estimate, 4), "\n")
-cat("p-value:", round(cor_test$p.value, 4), "\n")
+cat("p-value:", round(cor_test$p.value, 4), "\n") 
