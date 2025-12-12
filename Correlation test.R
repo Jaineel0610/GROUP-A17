b77cc0de17548d66faa043c3ab04b0f18ddc5e1a
Shanvitha.Vanam <sv24acc@herts.ac.uk>
2025-12-12 14:41:52 +0000

    Add files via upload

---
+++ Correlation test.R
@@ -0,0 +1,5 @@
+# 4. Correlation test
+cor_test <- cor.test(data_clean$price, data_clean$rating, method = "pearson")
+cat("\nPearson Correlation Test:\n")
+cat("Correlation coefficient (r):", round(cor_test$estimate, 4), "\n")
+cat("p-value:", round(cor_test$p.value, 4), "\n")
